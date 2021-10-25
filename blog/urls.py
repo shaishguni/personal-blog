216fab5668d367e7bbb15c7a9d0523b1a30b8af2
Shaish Guni <gunisguni2007@gmail.com>
2021-10-25 11:41:59 +0545

    made all backend part left to make frontend and custom django admin panel:)

--- blog/urls.py
+++ blog/urls.py
@@ -1,18 +1,16 @@
 from django.conf.urls.static import static
-from django.urls import include,path
 from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 
-
-urlspatterns = [
-    path('admin/',admin.site.urls),
-    path('ckeditor/',include("ckeditor_uploaders.urls")),
-    path('',include("core.urls","core"),namespace = "content"),
-    path('accounts/',include("django.contrib.auth.urls")),
-    
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', include(('core.urls', 'core'), namespace='core')),
+    path('accounts/', include("django.contrib.auth.urls"))
 ]
 
-
 if settings.DEBUG:
-    urlspatterns = urlspatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlspatterns = urlspatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
