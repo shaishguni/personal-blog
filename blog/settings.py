75fb74ff0f017abb2dfd3f4c85baf5cf639c1adb
Shaish Guni <gunisguni2007@gmail.com>
2021-11-27 23:54:13 +0545

    made kind of deployment ready :) xD

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ def sercret_key():
 
 SECRET_KEY = sercret_key()	
 # SECURITY WARNING: don't run with debug turned on in production!	
-DEBUG = False	
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','shaishguni-blog.herokuapp.com']	
 
